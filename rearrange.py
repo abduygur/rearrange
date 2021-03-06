2260c63ded2e30d37e91149bd82daf4e217a935e
Uygur <abduygur@gmail.com>
2021-03-07 00:16:53 +0300

    rearrange.py created

--- rearrange.py
+++ rearrange.py
@@ -1,1 +1,10 @@
+import re
 
+def rearrange_name(name):
+    result = re.search(r"^([\w \.-]*), ([\w \.-]*)$", name)
+    if result is None:
+        return name
+    return "{} {}".format(result[2], result[1])
+
+if __name__ == "__main__":
+    print(rearrange_name("Yigit, A. Uygur"))
